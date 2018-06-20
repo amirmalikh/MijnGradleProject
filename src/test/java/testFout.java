af62ee1a14751685ad1a1d86af5cda92fce64374
vdabcursist <vdabcursist@iMac-100.local>
2018-06-20 08:57:27 +0200

    t

--- src/test/java/testFout.java
+++ src/test/java/testFout.java
@@ -1,5 +1,20 @@
-public class testttt {
-    private int a ;
-    private int b ;
+import fraction.Breuk;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testFout {
+    private Breuk a ;
+    private Breuk b ;
+    private Breuk c ;
+
+    @Test
+    public void breukenOptellenFout() {
+        c = a.plus(b);
+        Assert.assertEquals(25,c.getTeller());
+        Assert.assertEquals(5,c.getNoemer());
+    }
+
+
+
 
 }
